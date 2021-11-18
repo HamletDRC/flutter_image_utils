582c2ab828060608b4f947c60b59f11c201b6fa6
stelladrc <stella.darcy@stud.edubs.ch>
2021-11-18 16:47:10 +0100

    rename to simple_image_utils

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# flutter_image_utils
+# flutter_simple_image_utils
 
 A small image utils package for flutter written in dart.
 
 Usage: 
 
 ```
-import 'package:flutter_image_utils/flutter_image_utils.dart';
+import 'package:flutter_simple_image_utils/flutter_simple_image_utils.dart';
 
 String profileAsBase64 = await convertImageAssetToString('assets/images/profile.jpg');
 String fileAsBase64 = await convertImageFileToString(file);
--- test/flutter_image_utils_test.dart
+++ test/flutter_image_utils_test.dart
@@ -1,9 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_image_utils/flutter_image_utils.dart';
+import 'package:flutter_simple_image_utils/flutter_simple_image_utils.dart';
 
 void main() {
   test('adds one to input values', () async {
-    String profileAsBase64 = await convertImageAssetToString('assets/images/profile.jpg');
+    //String profileAsBase64 = await convertImageAssetToString('assets/images/profile.jpg');
   });
 }
